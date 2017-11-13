7f3af5faacea25a9ba806bc89aa39e04f318f4ea
Firvalian <taillinth@gmail.com>
2017-11-13 16:18:46 -0500

    Initial commit

---
+++ html/src/com/lipomancer/wwrp/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.lipomancer.wwrp.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.lipomancer.wwrp.WWRPGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new WWRPGame();
+        }
+}
