7b1a43004d31cd5c414b6fda04e695007196b777
Baron Von Clop <baronvonclop@gmail.com>
2017-11-14 17:22:20 -0500

    Adding missing methods.

--- core/src/com/lipomancer/wwrp/game/ZoneImpl.java
+++ core/src/com/lipomancer/wwrp/game/ZoneImpl.java
@@ -1,4 +1,16 @@
 package com.lipomancer.wwrp.game;
 
+import java.util.List;
+
 public class ZoneImpl implements Zone{
+
+    @Override
+    public List<List<ZoneCell>> getCells() {
+        return null;
+    }
+
+    @Override
+    public ZoneCell getCellAt(int x, int y) {
+        return null;
+    }
 }
